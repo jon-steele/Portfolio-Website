516889227a047e3e39e3ddf4fe6e4378d055f8b5
Jon Steele <jonwhsteele@gmail.com>
2023-02-26 10:47:44 -0800

    Fixed header title issues

--- components/sections/header.js
+++ components/sections/header.js
@@ -7,13 +7,13 @@ export default function Header(){
   const [open, setOpen] = useState(false);
 
     return(
-    <header className='h-16 font-ds fixed w-screen top-0 z-50 flex justify-between items-center p-4 bg-gray-700 text-slate-100'>
+    <header className='h-16 font-ds fixed w-screen top-0 z-50 flex justify-between items-center p-4 bg-gray-700 text-slate-200'>
       
       {/* Title, Email & Resume */}
-      <div className='flex items-center'>
-        <Link className="px-4 h-16 text-2xl md:text-3xl hover:text-gray-700 hover:bg-slate-100 transition duration-500 ease-in-out mx-2" href='#home'>Jon Steele</Link>
-        <a href='/images/Jon_Resume.pdf' target="_blank" className='bg-slate-100 text-gray-700 p-2 rounded-md mx-2 '>Resume</a>
-        <a href='mailto:mrjonsteele@gmail.com' target="_blank" className='bg-slate-100 text-gray-700 p-2 rounded-md mx-2' rel="noreferrer">Email</a>
+      <div className='flex items-center justify-center'>
+        <Link className="p-4 text-2xl h-16 md:text-3xl hover:text-gray-700 hover:bg-slate-200 transition duration-500 ease-in-out" href='#home'>Jon Steele</Link>
+        <a href='/images/Jon_Resume.pdf' target="_blank" className='bg-slate-200 text-gray-700 p-2 rounded-md mx-2 '>Resume</a>
+        <a href='mailto:mrjonsteele@gmail.com' target="_blank" className='bg-slate-200 text-gray-700 p-2 rounded-md mx-2' rel="noreferrer">Email</a>
       </div>
 
       {/* FiMenu represents the hamburger menu. It remains hidden, unless the screen size is less than 1024 pixels*/}
@@ -21,11 +21,11 @@ export default function Header(){
 
       {/* Navigation links */}
       <nav className={`${open ? "block" : "hidden"} bg-gray-700 fixed right-0 top-16 lg:static flex flex-col lg:block lg:flex-row`}>
-        <Link className="p-4 text-2xl md:text-3xl hover:text-gray-700 hover:bg-slate-100 transition duration-500 ease-in-out" href='#projects'>Projects</Link>
-        <Link className="p-4 text-2xl md:text-3xl hover:text-gray-700 hover:bg-slate-100 transition duration-500 ease-in-out" href='#skills'>Skills</Link>
-        <Link className="p-4 text-2xl md:text-3xl hover:text-gray-700 hover:bg-slate-100 transition duration-500 ease-in-out" href='#about'>About</Link>
-        <Link className="p-4 text-2xl md:text-3xl hover:text-gray-700 hover:bg-slate-100 transition duration-500 ease-in-out" href='#hobbies'>Hobbies</Link>
-        <Link className="p-4 text-2xl md:text-3xl hover:text-gray-700 hover:bg-slate-100 transition duration-500 ease-in-out" href='#contact'>Links</Link>
+        <Link className="p-4 text-2xl md:text-3xl hover:text-gray-700 hover:bg-slate-200 transition duration-500 ease-in-out" href='#projects'>Projects</Link>
+        <Link className="p-4 text-2xl md:text-3xl hover:text-gray-700 hover:bg-slate-200 transition duration-500 ease-in-out" href='#skills'>Skills</Link>
+        <Link className="p-4 text-2xl md:text-3xl hover:text-gray-700 hover:bg-slate-200 transition duration-500 ease-in-out" href='#about'>About</Link>
+        <Link className="p-4 text-2xl md:text-3xl hover:text-gray-700 hover:bg-slate-200 transition duration-500 ease-in-out" href='#hobbies'>Hobbies</Link>
+        <Link className="p-4 text-2xl md:text-3xl hover:text-gray-700 hover:bg-slate-200 transition duration-500 ease-in-out" href='#contact'>Links</Link>
       </nav>
 
     </header>
