aefae410cb14fa735d671be5f4d022bfeb078ea6
Jon Steele <jonwhsteele@gmail.com>
2023-03-09 19:47:49 -0800

    Updated videos on projects section

--- components/sections/projects.js
+++ components/sections/projects.js
@@ -14,7 +14,7 @@ export default function Projects({className}) {
         {/* Project one */}
         <div className='flex flex-col items-left my-16'>
           <div className='flex flex-col justify-top'>
-            <h1 className='font-nun text-3xl text-center text-gray-200'>Voice operated flashcards</h1>
+            <h1 className='font-nun text-3xl text-center text-gray-200'>Voice operated flashcards (voci)</h1>
             <ul className='p-2 my-2 font-nun font-medium font-lg bg-slate-200 text-gray-700 list-disc list-inside rounded-md'>
               <li>Voice recognition & Text to speech</li>
               <li>MVC Design</li>
@@ -29,7 +29,7 @@ export default function Projects({className}) {
           </div>
           <div className='p-4'>
           <a href='https://voci.ca' target="_blank" rel="noreferrer">
-          <iframe src="https://www.youtube.com/embed/zXr1FKg41f4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>          </a>
+          <iframe className='w-full' src="https://www.youtube.com/embed/zXr1FKg41f4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>          </a>
             <div className='flex justify-evenly mt-4'>
               <a href='https://voci.ca' target="_blank" rel="noreferrer"><h1 className='bg-slate-200 text-gray-700 rounded-md p-2 mr-2'>voci.ca</h1></a>
               <a href='https://github.com/vociapp/voci' target="_blank" rel="noreferrer"><h1 className='bg-slate-200 text-gray-700 rounded-md p-2 ml-2'>Source</h1></a>
@@ -40,7 +40,7 @@ export default function Projects({className}) {
         {/* Project two */}
         <div className='flex flex-col items-left my-16'>
           <div className='flex flex-col justify-top'>
-            <h1 className='font-nun text-3xl text-center text-gray-200'>Finance projection</h1>
+            <h1 className='font-nun text-3xl text-center text-gray-200'>Finance Projection</h1>
             <ul className='p-2 my-2 font-nun font-medium font-lg bg-slate-200 text-gray-700 list-disc list-inside rounded-md'>
               <li>Compound interest calculator</li>
               <li>Income tax calculator</li>
@@ -54,8 +54,8 @@ export default function Projects({className}) {
             </p>
           </div>
           <div className='p-4'>
-            <Image className='' src={budgetpng} alt='image of budget app' />
-            <div className='flex justify-evenly mt-4'>
+          <iframe className='w-full' src="https://www.youtube.com/embed/ZjatZvDDbXk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          <div className='flex justify-evenly mt-4'>
               <a href='/Budget.jar' download target="_blank" rel="noreferrer"><h1 className='bg-slate-200 text-gray-700 rounded-md p-2 mr-2'>Download</h1></a>
               <a href='https://github.com/jon-steele/Budget-Projection' target="_blank" rel="noreferrer"><h1 className='bg-slate-200 text-gray-700 rounded-md p-2 ml-2'>Source</h1></a>
             </div>
