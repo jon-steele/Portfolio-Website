c86a7dfcc6de5808b200ace41b278575cf988c5b
Jon Steele <jonwhsteele@gmail.com>
2023-05-30 22:49:39 -0700

    Fixes syntax error

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import '@/styles/globals.css'
 import { AppProps } from 'next/app';
 import { Analytics } from '@vercel/analytics/react';
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
