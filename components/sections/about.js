7cd534105200d12a6cf6f078035964c69153dd0f
Jon Steele <jonwhsteele@gmail.com>
2023-07-03 17:47:03 -0700

    Updates text in about section

--- components/sections/about.js
+++ components/sections/about.js
@@ -6,11 +6,11 @@ export default function About({className}) {
 
             <p className="text-left font-nun text-xl md:text-2xl lg:text-3xl leading-10 !important font-normal break-words">
                 
-                In my studies, I excel in my algorithms & data structures courses, achieving a <em className="font-bold">4.0 GPA </em> 
-                in my last three semesters and landing my name on the <em className="font-bold">Dean&apos;s List twice. </em>
-                I have strong foundational knowledge in programming languages, design models, and time complexity.
+                In my studies, I excelled in my algorithms & data structures courses, achieving a <em className="font-bold">4.0 GPA </em> 
+                in my final three semesters.
+                I have strong foundational knowledge in web technologies, programming languages, design models, and time complexity.
                 In 2020, I was awarded <em className="font-bold">Employee of the Year</em> at my previous job as a lifeguard.
-                In my spare time, I enjoy learning about new technologies, and recently added React, Next, and Tailwind into my skillset.
+                In my spare time, I enjoy learning about new technologies, recently adding Docker into my skillset.
                 Technology is something that excites me, as I see the impact it can have on our lives.
                 If you think you can use my skills, please reach out to me via <a href="mailto:mrjonsteele@gmail.com">
                 email,</a> or call me at <a href="tel:+6049976356"><em className="font-bold">604-997-6356</em>.</a>
