2e7050f269e1848c5341c64e5812e6300294bbb9
Jon Steele <jonwhsteele@gmail.com>
2023-08-27 11:53:20 -0700

    Adds music links to hobbies section

--- components/sections/hobbies.js
+++ components/sections/hobbies.js
@@ -12,7 +12,14 @@ export default function Hobbies({className}){
           In my spare time, I play basketball with friends in local leagues, and previously
           <a className="text-blue-500" href="https://gowolfpack.ca/news/2017/4/21/wolfpack-mens-basketball-sign-second-recruit-for-2017-18-steele_26092" target="_blank" rel="noreferrer"> played for Thompson Rivers University. </a>  
           I have a German Shorthaired Pointer named Gigi that I take with me when I go mountain biking, hiking, and camping.
-          Additionally, I love to sing and play guitar and piano, and have been playing for over 10 years.
+          Additionally, I love writing songs, playing guitar, piano, and bass, and have music available on 
+          <a className="text-blue-500" href="https://open.spotify.com/artist/7bvMfA07It3yyukUvfLmEQ?si=S9iJ0yiSTTaXh6P6SK3ncw" target="_blank" rel="noreferrer"> 
+          Spotify 
+          </a>
+          and 
+          <a className="text-blue-500" href="https://music.apple.com/ca/artist/jon-steele/1584182764" target="_blank" rel="noreferrer"> 
+          Apple Music
+          </a>
         </p>
 
         <div className="flex flex-row justify-center mt-8 gap-8">
